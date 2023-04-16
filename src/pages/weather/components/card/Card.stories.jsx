733386fb8db7431c51f57a7754960cc792ac1c1c
Sebastian Mysakowski <contact@smysakowski.com>
2023-04-16 09:20:03 +0200

    structure v3

---
+++ src/pages/weather/components/card/Card.stories.jsx
@@ -0,0 +1,16 @@
+// Button.stories.ts|tsx
+
+import type { Meta } from "@storybook/react";
+
+import Card from "./Card";
+
+const meta = {
+  /* 👇 The title prop is optional.
+   * See https://storybook.js.org/docs/react/configure/overview#configure-story-loading
+   * to learn how to generate automatic titles
+   */
+  title: "Card",
+  component: Card,
+};
+
+export default meta;
